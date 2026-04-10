0d2c42bb23309f5beadf37f3dce8dc59b6d2a091
Joel Collins <joel.collins@prothea.tech>
2026-04-10 14:44:20 +0100

    chore: replace tailwind

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function setMode(mode) {
     document.body.setAttribute('data-mode', mode);
     document.getElementById('opt-ios').classList.toggle('active', mode === 'ios');
     document.getElementById('opt-gbc').classList.toggle('active', mode === 'gbc');
-    document.getElementById('ring-ios').style.display = mode === 'ios' ? '' : 'none';
-    document.getElementById('ring-gbc').style.display = mode === 'gbc' ? '' : 'none';
+    document.getElementById('ring-ios').style.display = mode === 'ios' ? 'flex' : 'none';
+    document.getElementById('ring-gbc').style.display = mode === 'gbc' ? 'block' : 'none';
     history.replaceState(null, '', mode === 'gbc' ? '?mode=gbc' : '?mode=ios');
 }
 
